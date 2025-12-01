162e4aef6fa25178f00e7511a9b14b7ecd610404
Stanislaw Gruszka <stf_xl@wp.pl>
2025-12-01 12:37:36 +0100

    feat: filetype detect base of hashbang '#!/usr/bin/env bpftrace'

--- setup.lua
+++ setup.lua
@@ -2,6 +2,17 @@ vim.filetype.add({
   extension = {
     bt = "bpftrace"
   },
+  pattern = {
+    [".*"] = {
+      function(path, bufnr)
+        local first_line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ''
+        if vim.regex([[^#!.*bpftrace]]):match_str(first_line) ~= nil then
+          return "bpftrace"
+        end
+      end,
+      { priority = -math.huge }
+    }
+  }
 })
 
 local client = vim.lsp.start_client({
