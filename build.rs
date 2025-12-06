6fa158fce40b8517fe487d0f514b785b1ac37920
Stanislaw Gruszka <stf_xl@wp.pl>
2025-12-06 10:13:27 +0100

    build: add probes json entries generator
    Generate probes json completion entries from language.md

--- build.rs
+++ build.rs
@@ -3,7 +3,9 @@ use std::process::Command;
 fn main() {
     println!("cargo:rerun-if-changed=build/stdlib.md");
     println!("cargo:rerun-if-changed=build/gen_completion_stdlib.py");
-    println!("cargo:rerun-if-changed=src/gen/");
+
+    println!("cargo:rerun-if-changed=build/language.md");
+    println!("cargo:rerun-if-changed=build/gen_completion_probes.py");
 
     let status = Command::new("build/gen_completion_stdlib.py")
         .arg("build/stdlib.md")
@@ -12,4 +14,18 @@ fn main() {
         .expect("Failed to run Python script");
 
     assert!(status.success(), "Code generation failed");
+
+    let status = Command::new("build/gen_completion_probes.py")
+        .arg("build/language.md")
+        .arg("src/gen/completion_probes.rs")
+        .status()
+        .expect("Failed to run Python script");
+
+    assert!(status.success(), "Code generation failed");
+
+    // TODO: how to detect when gen files were removed,
+    //       but not force to rebuild every time?
+    //
+    // println!("cargo:rerun-if-changed=src/gen/completion_stdlib.rs");
+    // println!("cargo:rerun-if-changed=src/gen/completion_probes.rs");
 }
