80b1500a3213c60fa44d7832ffaad05fef4dad22
Stanislaw Gruszka <stf_xl@wp.pl>
2025-12-12 20:37:42 +0100

    refactor(parser): simplify is_argument with and_then

--- src/parser.rs
+++ src/parser.rs
@@ -216,12 +216,14 @@ fn is_args(line_str: &str, char_nr: usize) -> bool {
 
 pub fn is_argument(line_str: &str, char_nr: usize, args: &mut String) -> bool {
     let mut res = false;
-    if let Some(line_upto_char) = line_str.get(0..=char_nr) {
-        if let Some(last_word) = line_upto_char.rsplit([' ', '{', '(']).next() {
-            if last_word.starts_with("args.") {
-                args.push_str(last_word);
-                res = true;
-            }
+
+    if let Some(last_word) = line_str
+        .get(0..=char_nr)
+        .and_then(|line_upto_char| line_upto_char.rsplit([' ', '{', '(']).next())
+    {
+        if last_word.starts_with("args.") {
+            args.push_str(last_word);
+            res = true;
         }
     }
 
